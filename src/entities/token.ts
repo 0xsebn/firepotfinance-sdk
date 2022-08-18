52f3114b283c42b59ab7f3afb573e5421b3416d8
sebn <sebn@firepot.finance>
2022-08-18 08:25:13 -0500

    Updated currency symbol

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -62,22 +62,22 @@ export const WETH = {
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     18,
     'WETH',
-    'Wrapped Ether'
+    'Wrapped Amber'
   ),
   [ChainId.ROPSTEN]: new Token(
     ChainId.ROPSTEN,
     '0xc778417E063141139Fce010982780140Aa0cD5Ab',
     18,
     'WETH',
-    'Wrapped Ether'
+    'Wrapped Amber'
   ),
   [ChainId.RINKEBY]: new Token(
     ChainId.RINKEBY,
     '0xc778417E063141139Fce010982780140Aa0cD5Ab',
     18,
     'WETH',
-    'Wrapped Ether'
+    'Wrapped Amber'
   ),
-  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Amber'),
   [ChainId.AMBTEST]: new Token(ChainId.AMBTEST, '0x55C402b5F9C2c3DfE3d866B36598f0Fd53e03B89', 18, 'SAMB', 'Synthetic Amber')
 }
